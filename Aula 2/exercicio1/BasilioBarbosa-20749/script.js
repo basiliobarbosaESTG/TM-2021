ba911e68cdf4195c6dddad4d2546fc452bd4f38e
basiliobarbosaESTG <79463228+basiliobarbosaESTG@users.noreply.github.com>
2021-03-01 14:52:38 +0000

    Exercicio1Correction

--- Aula 2/exercicio1/BasilioBarbosa-20749/script.js
+++ Aula 2/exercicio1/BasilioBarbosa-20749/script.js
@@ -1,10 +1,10 @@
 // Your JS Script here
 
-function readInput(){
-    const text = document.getElementById('texto').value
-    if(!text){
-        alert("Escreva o texto no input")
-    } else{
-    document.getElementById('textoFromInput').innerText = text
+function readInput() {
+    const text = document.getElementById('texto').value;
+    if (!text) {
+        alert("Escreva o texto no input");
+    } else {
+        document.getElementById('textoFromInput').innerText = text;
     }
 }
